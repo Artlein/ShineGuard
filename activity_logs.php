bca93242fa63dab0e4b83b6fecbab050a9dfe9d0
ShineGuard Dev <shineguard@example.com>
2026-04-21 00:03:11 +0800

    Fix: Restored parsing UI layout for activity logs that was broken by missing PHP tag

--- activity_logs.php
+++ activity_logs.php
@@ -630,6 +630,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['verify_integrity'])) 
                                             <?php endif; ?>
                                         </td>
                                         <td class="details-col">
+                                            <?php
                                                 $details = $log['details'];
                                                 $device_token = null;
                                                 
