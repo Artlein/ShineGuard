e357296d23347b9e89244f3cd97f440f7d517633
ShineGuard Dev <shineguard@example.com>
2026-04-21 21:46:43 +0800

    Clean: Remove emergency diagnostic scripts

--- scratch/check_schema.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'dbconnect.php';
-$res = $conn->query("DESCRIBE user_devices");
-while ($row = $res->fetch_assoc()) {
-    print_r($row);
-}
-?>
--- scratch/verify_time.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'dbconnect.php';
-$res = $conn->query("SELECT @@session.time_zone as tz, NOW() as now");
-$row = $res->fetch_assoc();
-echo "MySQL Session Timezone: " . $row['tz'] . "\n";
-echo "MySQL NOW(): " . $row['now'] . "\n";
-echo "PHP date(): " . date('Y-m-d H:i:s') . "\n";
-?>
