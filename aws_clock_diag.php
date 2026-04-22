a24ad7da9530ac242e309cba090fcf63700327ec
ShineGuard Dev <shineguard@example.com>
2026-04-22 16:23:24 +0800

    CLEANUP: Removed 18 obsolete simulators, diagnostic tools, and redundant backups

--- aws_clock_diag.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-date_default_timezone_set('Asia/Manila');
-echo json_encode([
-    'server_time' => date('Y-m-d H:i:s'),
-    'timestamp' => time(),
-    'timezone' => date_default_timezone_get()
-]);
-?>
--- session_clear.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'dbconnect.php';
-session_unset();
-session_destroy();
-header('Location: login.php?msg=session_cleared');
-exit();
-?>
